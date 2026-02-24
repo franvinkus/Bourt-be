e01c45269681c003be7feb2651442212a189e8fd
franvinkus <vincentkurnia29@gmail.com>
2026-02-24 21:54:54 +0700

    Update on Booking Entities

--- Models/Booking.cs
+++ Models/Booking.cs
@@ -3,12 +3,14 @@
     public class Booking
     {
         public Guid Id { get; set; }
-        public Guid CoursDetailsId { get; set; }
-        public Guid UserId { get; set; }
+        public Court Court { get; set; }
+        public Guid CourtId { get; set; }
         public User User { get; set; }
+        public Guid UserId { get; set; }
         public decimal TotalPrice { get; set; }
-        public DateTime StartTime { get; set; }
-        public DateTime EndTime { get; set; }
+        public DateOnly Date {  get; set; }
+        public TimeOnly StartTime { get; set; }
+        public TimeOnly EndTime { get; set; }
         public DateTime CreatedAt { get; set; }
         public DateTime UpdatedAt { get; set; }
         public Enums.BookingStatus Status { get; set; }
